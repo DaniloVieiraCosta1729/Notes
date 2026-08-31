54e7a26afc2b921d7f0fdbababce9950f059dc67
Danilo Vieria Costa <danilo.11235@outlook.com>
2026-08-31 05:48:39 -0300

    add the initial version of the server and change the main page to fetch the note content only after the user clicks on what he wants to see.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,31 @@
-// I'm don't use JS very often, so I'll commenting the things I've learned while I write this.
-// To take only a single function from node, I can use destructuring.
-const {createServer} = require('node:http'); // I could make const http = require('node:http'); but the destructuring, like I said, allows me to take only the createServer function.
+const {createServer} = require('node:http');
 const {join} = require('path');
 const {createReadStream} = require('fs');
 
-const host = '127.0.0.1';
-const port = 5000;
+const root = join(__dirname, '../');
+
+function sendContent(response, url){
+
+    let content;
+    let stream;
+    
+    if (url === '/') {
+        content = join(root, 'index.html');
+        stream = createReadStream(content);
+        stream.pipe(response);
+    } else {
+        content = join(root, `/${url}`);
+        stream = createReadStream(content);
+        stream.pipe(response);
+    }
+}
 
 const server = createServer(
+    (request, response) => {
+        const {url} = request;
 
-    (req, res) => {
-        res.statusCode = 200; 
-        res.setHeader('Content-Type', 'audio/mpeg');
-
-        const sound = join(__dirname,"../static/audio/hallo_worldoo.mp3");
-
-        const stream = createReadStream(sound);
-        stream.pipe(res);
+        sendContent(response, url);
     }
-
 );
 
-server.listen(port, host, () => {console.log(`listen on ${host}:${port}`)})
+server.listen(5000);
