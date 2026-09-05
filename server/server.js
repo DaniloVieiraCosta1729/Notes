296bf546c5cfcc7ce29e0d06a0cf77d8012b803f
Danilo Vieria Costa <danilo.11235@outlook.com>
2026-09-04 21:52:19 -0300

    improves layout, adds capabilities (write directly in the page and save the content).

--- server/server.js
+++ server/server.js
@@ -1,20 +1,37 @@
-const {createServer} = require('node:http');
+const {createServer, request} = require('node:http');
 const {join} = require('path');
 const {createReadStream} = require('fs');
+const { writeFile } = require('node:fs/promises');
+
+async function savaHTML(request, path)
+{
+    let text = [];
+
+    await request.on('data', chunk => {text.push(chunk)}).on('end', () => text = Buffer.concat(text).toString());
+
+    writeFile(path, text, 'utf8');
+}
 
 const root = join(__dirname, '../');
+let currentNote = '';
 
-function sendContent(response, url){
+function sendContent(response, request, url){
 
-    let content;
     let stream;
+    let content;
     
     if (url === '/') {
         content = join(root, 'index.html');
+        currentNote = content;
         stream = createReadStream(content);
         stream.pipe(response);
+    } else if(url === '/save'){
+        console.log(`${currentNote}`);
+        savaHTML(request, currentNote);
     } else {
         content = join(root, `/${url}`);
+        currentNote = content;
+        console.log(`caderno atual: ${currentNote}`);
         stream = createReadStream(content);
         stream.pipe(response);
     }
@@ -24,7 +41,7 @@ const server = createServer(
     (request, response) => {
         const {url} = request;
 
-        sendContent(response, url);
+        sendContent(response, request, url);
     }
 );
 
